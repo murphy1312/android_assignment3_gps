37fcff52f014a2310ec54f1b807b118e0d4f8d12
andreas hulsch <andreas@hulsch.eu>
2016-04-18 15:10:46 +0100

    end lab 18/04

--- app/src/main/java/eu/hulsch/andreas/gpstracker/GpsGraphCustomView.java
+++ app/src/main/java/eu/hulsch/andreas/gpstracker/GpsGraphCustomView.java
@@ -1,8 +1,33 @@
 package eu.hulsch.andreas.gpstracker;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by Andreas Hulsch on 4/18/2016.
  */
-public class GpsGraphCustomView
+public class GpsGraphCustomView extends View
 {
+    public GpsGraphCustomView(Context context)
+    {
+        super(context);
+        init();
+    }
+    public GpsGraphCustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+    public GpsGraphCustomView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init()
+    {
+
+    }
+
+
 }
--- app/src/main/java/eu/hulsch/andreas/gpstracker/MainActivity.java
+++ app/src/main/java/eu/hulsch/andreas/gpstracker/MainActivity.java
@@ -2,12 +2,36 @@ package eu.hulsch.andreas.gpstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView gps_active_tv;
+    private TextView current_speed_tv;
+    private TextView average_speed_tv;
+    private TextView overall_time_tv;
+    private Button tracking_btn;
+    private GpsGraphCustomView gpsGraphCustomView;
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews()
+    {
+        gps_active_tv = (TextView) findViewById(R.id.gps_active);
+        current_speed_tv = (TextView) findViewById(R.id.current_speed);
+        average_speed_tv = (TextView) findViewById(R.id.average_speed);
+        overall_time_tv = (TextView) findViewById(R.id.overall_time);
+        tracking_btn = (Button) findViewById(R.id.btn_tracking);
+        gpsGraphCustomView = (GpsGraphCustomView) findViewById(R.id.gps_graph_custom_view);
     }
 }
